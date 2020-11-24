2da0081aa34e6b9d9a6708df5db076836638b9cc
HuangxinYu359 <181150016@hdu.edu.cn>
2020-11-24 16:09:02 +0800

    11.23update

--- nn/numpy模拟神经网络/numpy模拟单个神经元.py
+++ nn/numpy模拟神经网络/numpy模拟单个神经元.py
@@ -2,18 +2,18 @@
 import numpy as np
 
 #n为样本大小,d_in为输入层维度，h为隐藏层维度（只有一层隐藏层）d_out为输出层维度
-n,d_in,h,d_out = 64,100,50,2
+n,d_in,h,d_out = 1,1,1,1
 
 #随机生成数据
-x = np.random.randn(n,d_in)
-y = np.random.randn(n,d_out)
+x = np.array([100])
+y = np.array([200])
 #print(x,y)
 
 #随机初始化权重
 #输入层到隐藏层（100，50）
-w1 = np.random.randn(d_in,h)
+w1 = np.array([1.5])
 #隐藏层到输出层（50，1）
-w2 = np.random.randn(h,d_out)
+w2 = np.array([1.5])
 #设置学习率
 learning_rate = 1e-6
 
@@ -21,8 +21,8 @@ learning_rate = 1e-6
 for i in range(100):
     #前向传播
     temp = np.dot(x,w1)
-    temp_relu = np.maximum(temp,0)
-    y_pred = np.dot(temp_relu,w2)
+
+    y_pred = np.dot(temp,w2)
 
     #计算前向传播的损失函数
     loss = np.square(y_pred-y).sum()/n
@@ -30,14 +30,11 @@ for i in range(100):
 
     #基于loss梯度，反向传播
     grad_y_pred = 2.0*(y_pred-y)
-    grad_w2 = np.dot(temp_relu.T,grad_y_pred)
+    grad_w2 = np.dot(temp.T,grad_y_pred)
     grad_temp_relu = np.dot(grad_y_pred,w2.T)
     grad_temp = grad_temp_relu.copy()
-    grad_temp_relu[temp<0] = 0
     grad_w1 = x.T.dot(grad_temp)
 
     # 更新权重
     w1 -= learning_rate * grad_w1
     w2 -= learning_rate * grad_w2
-
-
